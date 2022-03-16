1b9274b7c947690fa39e45552286f4f787831a1e
Ben Thomasson <bthomass@redhat.com>
2022-03-16 13:58:10 -0400

    Fixing version

--- ansible_events/__init__.py
+++ ansible_events/__init__.py
@@ -1,5 +1,5 @@
-"""Top-level package for FTL Events."""
+"""Top-level package for Ansible Events."""
 
 __author__ = """Ben Thomasson"""
 __email__ = 'ben.thomasson@gmail.com'
-__version__ = '0.1.9'
+__version__ = '0.1.0'
