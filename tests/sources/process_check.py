5ecbe4190979fdfe06d40c6a23cc744dcfaf263d
Ben Thomasson <bthomass@redhat.com>
2022-04-01 15:46:30 -0400

    Adds more test sources

--- tests/sources/process_check.py
+++ tests/sources/process_check.py
@@ -1,42 +1,49 @@
-
-
 import psutil
 import time
 
 
 def main(queue, args):
 
-    names = args.get('names', None)
+    names = args.get("names", None)
 
     original_processes = {p.pid: p.name() for p in psutil.process_iter()}
     if names is not None:
-        original_processes = {pid: name for pid, name in original_processes.items() if name in names}
+        original_processes = {
+            pid: name for pid, name in original_processes.items() if name in names
+        }
 
     while True:
         time.sleep(1)
         current_processes = {p.pid: p.name() for p in psutil.process_iter()}
         if names is not None:
-            current_processes = {pid: name for pid, name in current_processes.items() if name in names}
+            current_processes = {
+                pid: name for pid, name in current_processes.items() if name in names
+            }
         new_processes = set(current_processes.keys()) - set(original_processes.keys())
         lost_processes = set(original_processes.keys()) - set(current_processes.keys())
         for new in new_processes:
-            queue.put(dict(process_check=dict(pid=new,
-                           name=current_processes[new],
-                           status='running'
-                           )))
+            queue.put(
+                dict(
+                    process_check=dict(
+                        pid=new, name=current_processes[new], status="running"
+                    )
+                )
+            )
         for lost in lost_processes:
-            queue.put(dict(process_check=dict(event_type='lost_process',
-                           pid=lost,
-                           name=original_processes[lost],
-                           status='stopped'
-                          )))
+            queue.put(
+                dict(
+                    process_check=dict(
+                        pid=lost, name=original_processes[lost], status="stopped"
+                    )
+                )
+            )
         original_processes = current_processes
 
 
-
 if __name__ == "__main__":
+
     class MockQueue:
         def put(self, event):
             print(event)
-    main(MockQueue(), {})
 
+    main(MockQueue(), {})
