a78c5e5b5f1e626b1c11b854823401bd24298cea
Ben Thomasson <bthomass@redhat.com>
2022-05-02 13:34:15 -0400

    Bump version: 0.1.2 → 0.2.0

--- ansible_events/__init__.py
+++ ansible_events/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Ben Thomasson"""
 __email__ = 'ben.thomasson@gmail.com'
-__version__ = '0.1.2'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/benthomasson/ansible-events',
-    version='0.1.2',
+    version='0.2.0',
     zip_safe=False,
 )
