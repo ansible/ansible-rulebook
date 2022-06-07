faf67cdc3c7fb9d8aa90f71095ab249071fb32e3
Ben Thomasson <bthomass@redhat.com>
2022-06-07 16:52:06 -0400

    Catches websocket errors

--- ansible_events/websocket.py
+++ ansible_events/websocket.py
@@ -3,13 +3,20 @@
 import asyncio
 import websockets
 import json
+import logging
 
 
 async def send_event_log_to_websocket(event_log, websocket_address):
+    logger = logging.getLogger()
     async for websocket in websockets.connect(websocket_address):
+        event = None
         try:
             while True:
                 event = await event_log.get()
                 await websocket.send(json.dumps(event))
         except websockets.ConnectionClosed:
+            logger.warning(f'websocket {websocket_address} connection closed')
+            continue
+        except BaseException as e:
+            logger.error(f'websocket error {e} on {event}')
             continue
