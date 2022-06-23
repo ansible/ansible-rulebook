c625c48a2d55fb99e3eb17371b7c9a7436e7aba0
Ben Thomasson <bthomass@redhat.com>
2022-06-23 09:43:28 -0400

    Bump version: 0.3.0 → 0.4.0

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/benthomasson/ansible-events',
-    version='0.3.0',
+    version='0.4.0',
     zip_safe=False,
 )
