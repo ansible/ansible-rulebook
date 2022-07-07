96518a7c3ee5f1841ef0bdf4fc12032f02a13b6b
Ben Thomasson <bthomass@redhat.com>
2022-07-07 15:41:49 -0400

    Adds examples for multiple sources

---
+++ tests/sources/range2.py
@@ -0,0 +1,13 @@
+def main(queue, args):
+
+    for i in range(int(args["limit"])):
+        queue.put(dict(range2=dict(i=i)))
+
+
+if __name__ == "__main__":
+
+    class MockQueue:
+        def put(self, event):
+            print(event)
+
+    main(MockQueue(), dict(limit=5))
