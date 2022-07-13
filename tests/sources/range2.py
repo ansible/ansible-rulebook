a319de5f284f261e98717c5d5e7542fba51bc693
Bill Wei <bilwei@redhat.com>
2022-07-13 14:54:31 -0400

    Fix async sources of hosts and range2

--- tests/sources/range2.py
+++ tests/sources/range2.py
@@ -1,13 +1,17 @@
-def main(queue, args):
+import asyncio
+from typing import Any, Dict
+
+
+async def main(queue: asyncio.Queue, args: Dict[str, Any]):
 
     for i in range(int(args["limit"])):
-        queue.put(dict(range2=dict(i=i)))
+        await queue.put(dict(range2=dict(i=i)))
 
 
 if __name__ == "__main__":
 
     class MockQueue:
-        def put(self, event):
+        async def put(self, event):
             print(event)
 
-    main(MockQueue(), dict(limit=5))
+    asyncio.run(main(MockQueue(), dict(limit=5)))
