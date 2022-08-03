9350001af69e3d2b5a9c21fedccc6a43c4012aec
Edoardo Vacchi <evacchi@users.noreply.github.com>
2022-08-03 17:28:08 +0200

    use single underscore for private members, replace exception with list check

--- ansible_events/drools/durable_rules_engine.py
+++ ansible_events/drools/durable_rules_engine.py
@@ -7,17 +7,17 @@ _logger = logging.getLogger(__name__)
 
 
 class DurableRulesEngine:
-    __host = None
-    __last_resp = None
+    _host = None
+    _last_resp = None
 
     def __init__(self, host):
-        self.__host = host
+        self._host = host
 
     def create_ruleset(self, ruleset_name, ruleset_string):
         req = {ruleset_name: json.loads(ruleset_string)}
 
         r = requests.post(
-            self.__host + "/create-durable-rules-executor", json=req
+            self._host + "/create-durable-rules-executor", json=req
         )
         if r.status_code != 200:
             raise Exception(
@@ -40,7 +40,7 @@ class DurableRulesEngine:
         serialized_fact = json.dumps(d)
 
         r = requests.post(
-            f"{self.__host}/rules-durable-executors/{session_id}/process",
+            f"{self._host}/rules-durable-executors/{session_id}/process",
             json=json.loads(serialized_fact),
         )
 
@@ -50,31 +50,33 @@ class DurableRulesEngine:
                 + json.loads(r.content)["details"]
             )
 
-        self.__last_resp = r.json()
-        self.__last_resp.reverse()
+        self._last_resp = r.json()
+        self._last_resp.reverse()
 
         return (0, session_id)
 
     def start_action_for_state(self, handle):  # real signature unknown
-        try:
-            resp = self.__last_resp.pop()
-        except Exception:
+
+        if self._last_resp:
+            resp = self._last_resp.pop()
+        else:
             return None
 
         return ('{ "sid":"0", "id":"sid-0", "$s":1}', json.dumps(resp), handle)
 
     def complete_and_start_action(self, handle):  # real signature unknown
         _logger.info("complete_and_start_action: %d", handle)
-        try:
-            resp = self.__last_resp.pop()
-        except Exception:
+
+        if self._last_resp:
+            resp = self._last_resp.pop()
+        else:
             return None
 
         return json.dumps(resp)
 
     def retract_fact(self, session_id, serialized_fact):
         r = requests.post(
-            f"{self.__host}/rules-durable-executors/{session_id}/retract-fact",
+            f"{self._host}/rules-durable-executors/{session_id}/retract-fact",
             json=json.loads(serialized_fact),
         )
         if r.status_code != 200:
@@ -87,7 +89,7 @@ class DurableRulesEngine:
 
     def get_facts(self, session_id, _sid):
         r = requests.post(
-            f"{self.__host}/rules-durable-executors/{session_id}/get-all-facts"
+            f"{self._host}/rules-durable-executors/{session_id}/get-all-facts"
         )
         if r.status_code != 200:
             raise Exception(
@@ -111,7 +113,7 @@ class error(Exception):
 
 # exported methods
 
-__instance = DurableRulesEngine("http://localhost:8080")
+_instance = DurableRulesEngine("http://localhost:8080")
 
 
 def abandon_action(*args, **kwargs):  # real signature unknown
@@ -119,7 +121,7 @@ def abandon_action(*args, **kwargs):  # real signature unknown
 
 
 def assert_event(session_id, serialized_fact):
-    return __instance.assert_fact(session_id, serialized_fact)
+    return _instance.assert_fact(session_id, serialized_fact)
 
 
 def assert_events(*args, **kwargs):  # real signature unknown
@@ -127,7 +129,7 @@ def assert_events(*args, **kwargs):  # real signature unknown
 
 
 def assert_fact(session_id, serialized_fact):
-    return __instance.assert_fact(session_id, serialized_fact)
+    return _instance.assert_fact(session_id, serialized_fact)
 
 
 def assert_facts(*args, **kwargs):  # real signature unknown
@@ -151,7 +153,7 @@ def complete_get_queued_messages(*args, **kwargs):  # real signature unknown
 
 
 def create_ruleset(ruleset_name, ruleset_string):
-    return __instance.create_ruleset(ruleset_name, ruleset_string)
+    return _instance.create_ruleset(ruleset_name, ruleset_string)
 
 
 def delete_ruleset(*args, **kwargs):  # real signature unknown
@@ -168,7 +170,7 @@ def get_events(*args, **kwargs):  # real signature unknown
 
 
 def get_facts(handle, session_id):  # real signature unknown
-    return __instance.get_facts(handle, session_id)
+    return _instance.get_facts(handle, session_id)
 
 
 def get_state(*args, **kwargs):  # real signature unknown
@@ -180,7 +182,7 @@ def renew_action_lease(*args, **kwargs):  # real signature unknown
 
 
 def retract_fact(handle, payload):
-    return __instance.retract_fact(handle, payload)
+    return _instance.retract_fact(handle, payload)
 
 
 def retract_facts(*args, **kwargs):  # real signature unknown
@@ -215,11 +217,11 @@ def start_action(*args, **kwargs):
 
 
 def start_action_for_state(handle, state_handle):
-    return __instance.start_action_for_state(handle)
+    return _instance.start_action_for_state(handle)
 
 
 def complete_and_start_action(handle, context_handle):
-    return __instance.complete_and_start_action(handle)
+    return _instance.complete_and_start_action(handle)
 
 
 def start_timer(*args, **kwargs):  # real signature unknown
