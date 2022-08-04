304ed4d3f407c7d935b5dc01c1569585b87b3fdc
Madhu Kanoor <mkanoor@redhat.com>
2022-08-04 16:59:05 -0400

    Fixed the source in performance_test to be asyncio compatible
    We had fixed this in tests/sources but not in preformance_test/sources

--- performance_test/sources/range.py
+++ performance_test/sources/range.py
@@ -1,4 +1,19 @@
-def main(queue, args):
+import asyncio
+from typing import Any, Dict
+
+
+async def main(queue: asyncio.Queue, args: Dict[str, Any]):
+    delay = args.get("delay", 0)
 
     for i in range(int(args["limit"])):
-        queue.put(dict(i=i))
+        await queue.put(dict(i=i))
+        await asyncio.sleep(delay)
+
+
+if __name__ == "__main__":
+
+    class MockQueue:
+        async def put(self, event):
+            print(event)
+
+    asyncio.run(main(MockQueue(), dict(limit=5)))
