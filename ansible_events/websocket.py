12fd7e53c177df5580893a0672f32abe2954db1f
Ben Thomasson <bthomass@redhat.com>
2022-08-22 12:09:18 -0400

    Moves logger init

--- ansible_events/websocket.py
+++ ansible_events/websocket.py
@@ -4,12 +4,13 @@ from asyncio.exceptions import CancelledError
 
 import websockets
 
+logger = logging.getLogger("ansible_events.websocket")
+
 
 async def send_event_log_to_websocket(event_log, websocket_address):
-    logger = logging.getLogger("ansible_events.websocket")
-    logger.info(f"websocket %s connecting", websocket_address)
+    logger.info("websocket %s connecting", websocket_address)
     async for websocket in websockets.connect(websocket_address):
-        logger.info(f"websocket {websocket_address} connected")
+        logger.info("websocket %s connected", websocket_address)
         event = None
         try:
             while True:
@@ -18,11 +19,9 @@ async def send_event_log_to_websocket(event_log, websocket_address):
                 if event == dict(type="Shutdown"):
                     return
         except websockets.ConnectionClosed:
-            logger.warning(f"websocket {websocket_address} connection closed")
-            continue
+            logger.warning("websocket %s connection closed", websocket_address)
         except CancelledError:
             logger.warning("closing websocket due to task cancelled")
             return
         except BaseException as e:
             logger.error(f"websocket error {type(e)} {e} on {event}")
-            continue
