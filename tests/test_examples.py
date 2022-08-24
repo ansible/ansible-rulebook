8e32c58ee1a29c1af74ad7dc27d39f32c49ae5cf
Ben Thomasson <bthomass@redhat.com>
2022-08-24 12:58:14 -0400

    Working on multiple ruleset tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -10,8 +10,9 @@ from .test_engine import load_rules
 
 @pytest.mark.asyncio
 async def test_01_noop():
-    ruleset_queues, queue, event_log = load_rules("examples/01_noop.yml")
+    ruleset_queues, event_log = load_rules("examples/01_noop.yml")
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=1))
     queue.put_nowait(Shutdown())
 
@@ -34,8 +35,9 @@ async def test_01_noop():
 
 @pytest.mark.asyncio
 async def test_02_debug():
-    ruleset_queues, queue, event_log = load_rules("examples/02_debug.yml")
+    ruleset_queues, event_log = load_rules("examples/02_debug.yml")
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=1))
     queue.put_nowait(Shutdown())
 
@@ -58,10 +60,11 @@ async def test_02_debug():
 
 @pytest.mark.asyncio
 async def test_03_print_event():
-    ruleset_queues, queue, event_log = load_rules(
+    ruleset_queues, event_log = load_rules(
         "examples/03_print_event.yml"
     )
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=1))
     queue.put_nowait(Shutdown())
 
@@ -84,10 +87,11 @@ async def test_03_print_event():
 
 @pytest.mark.asyncio
 async def test_04_assert_fact():
-    ruleset_queues, queue, event_log = load_rules(
+    ruleset_queues, event_log = load_rules(
         "examples/04_assert_fact.yml"
     )
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=1))
     queue.put_nowait(Shutdown())
 
@@ -113,8 +117,9 @@ async def test_04_assert_fact():
 
 @pytest.mark.asyncio
 async def test_05_post_event():
-    ruleset_queues, queue, event_log = load_rules("examples/05_post_event.yml")
+    ruleset_queues, event_log = load_rules("examples/05_post_event.yml")
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=1))
     queue.put_nowait(Shutdown())
 
@@ -140,10 +145,11 @@ async def test_05_post_event():
 
 @pytest.mark.asyncio
 async def test_06_retract_fact():
-    ruleset_queues, queue, event_log = load_rules(
+    ruleset_queues, event_log = load_rules(
         "examples/06_retract_fact.yml"
     )
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=1))
     queue.put_nowait(Shutdown())
 
@@ -172,8 +178,9 @@ async def test_06_retract_fact():
 
 @pytest.mark.asyncio
 async def test_07_and():
-    ruleset_queues, queue, event_log = load_rules("examples/07_and.yml")
+    ruleset_queues, event_log = load_rules("examples/07_and.yml")
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(nested=dict(i=1, j=1)))
     queue.put_nowait(Shutdown())
 
@@ -196,8 +203,9 @@ async def test_07_and():
 
 @pytest.mark.asyncio
 async def test_08_or():
-    ruleset_queues, queue, event_log = load_rules("examples/08_or.yml")
+    ruleset_queues, event_log = load_rules("examples/08_or.yml")
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(nested=dict(i=1, j=1)))
     queue.put_nowait(Shutdown())
 
@@ -220,8 +228,9 @@ async def test_08_or():
 
 @pytest.mark.asyncio
 async def test_09_gt():
-    ruleset_queues, queue, event_log = load_rules("examples/09_gt.yml")
+    ruleset_queues, event_log = load_rules("examples/09_gt.yml")
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=3))
     queue.put_nowait(Shutdown())
 
@@ -244,8 +253,9 @@ async def test_09_gt():
 
 @pytest.mark.asyncio
 async def test_10_lt():
-    ruleset_queues, queue, event_log = load_rules("examples/10_lt.yml")
+    ruleset_queues, event_log = load_rules("examples/10_lt.yml")
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=1))
     queue.put_nowait(Shutdown())
 
@@ -268,8 +278,9 @@ async def test_10_lt():
 
 @pytest.mark.asyncio
 async def test_11_le():
-    ruleset_queues, queue, event_log = load_rules("examples/11_le.yml")
+    ruleset_queues, event_log = load_rules("examples/11_le.yml")
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=2))
     queue.put_nowait(Shutdown())
 
@@ -292,8 +303,9 @@ async def test_11_le():
 
 @pytest.mark.asyncio
 async def test_12_ge():
-    ruleset_queues, queue, event_log = load_rules("examples/12_ge.yml")
+    ruleset_queues, event_log = load_rules("examples/12_ge.yml")
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=2))
     queue.put_nowait(Shutdown())
 
@@ -316,8 +328,9 @@ async def test_12_ge():
 
 @pytest.mark.asyncio
 async def test_13_add():
-    ruleset_queues, queue, event_log = load_rules("examples/13_add.yml")
+    ruleset_queues, event_log = load_rules("examples/13_add.yml")
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(nested=dict(i=2, j=1)))
     queue.put_nowait(Shutdown())
 
@@ -340,8 +353,9 @@ async def test_13_add():
 
 @pytest.mark.asyncio
 async def test_14_sub():
-    ruleset_queues, queue, event_log = load_rules("examples/14_sub.yml")
+    ruleset_queues, event_log = load_rules("examples/14_sub.yml")
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(nested=dict(i=1, j=2)))
     queue.put_nowait(Shutdown())
 
@@ -364,10 +378,11 @@ async def test_14_sub():
 
 @pytest.mark.asyncio
 async def test_15_multiple_events_all():
-    ruleset_queues, queue, event_log = load_rules(
+    ruleset_queues, event_log = load_rules(
         "examples/15_multiple_events_all.yml"
     )
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(nested=dict(i=1, j=0)))
     queue.put_nowait(dict(nested=dict(i=0, j=1)))
     queue.put_nowait(Shutdown())
@@ -393,10 +408,11 @@ async def test_15_multiple_events_all():
 
 @pytest.mark.asyncio
 async def test_16_multiple_events_any():
-    ruleset_queues, queue, event_log = load_rules(
+    ruleset_queues, event_log = load_rules(
         "examples/16_multiple_events_any.yml"
     )
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(nested=dict(i=1, j=0)))
     queue.put_nowait(Shutdown())
 
@@ -419,10 +435,11 @@ async def test_16_multiple_events_any():
 
 @pytest.mark.asyncio
 async def test_17_multiple_sources_any():
-    ruleset_queues, queue, event_log = load_rules(
+    ruleset_queues, event_log = load_rules(
         "examples/17_multiple_sources_any.yml"
     )
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=1))
     queue.put_nowait(dict(range2=dict(i=1)))
     queue.put_nowait(Shutdown())
@@ -451,10 +468,11 @@ async def test_17_multiple_sources_any():
 
 @pytest.mark.asyncio
 async def test_18_multiple_sources_all():
-    ruleset_queues, queue, event_log = load_rules(
+    ruleset_queues, event_log = load_rules(
         "examples/18_multiple_sources_all.yml"
     )
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=1))
     queue.put_nowait(dict(range2=dict(i=1)))
     queue.put_nowait(Shutdown())
@@ -480,8 +498,9 @@ async def test_18_multiple_sources_all():
 
 @pytest.mark.asyncio
 async def test_19_is_defined():
-    ruleset_queues, queue, event_log = load_rules("examples/19_is_defined.yml")
+    ruleset_queues, event_log = load_rules("examples/19_is_defined.yml")
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=1))
     queue.put_nowait(Shutdown())
 
@@ -507,10 +526,11 @@ async def test_19_is_defined():
 
 @pytest.mark.asyncio
 async def test_20_is_not_defined():
-    ruleset_queues, queue, event_log = load_rules(
+    ruleset_queues, event_log = load_rules(
         "examples/20_is_not_defined.yml"
     )
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=1))
     queue.put_nowait(Shutdown())
 
@@ -539,10 +559,11 @@ async def test_20_is_not_defined():
 
 @pytest.mark.asyncio
 async def test_21_run_playbook():
-    ruleset_queues, queue, event_log = load_rules(
+    ruleset_queues, event_log = load_rules(
         "examples/21_run_playbook.yml"
     )
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=1))
     queue.put_nowait(Shutdown())
 
@@ -569,10 +590,11 @@ async def test_21_run_playbook():
 
 @pytest.mark.asyncio
 async def test_23_nested_data():
-    ruleset_queues, queue, event_log = load_rules(
+    ruleset_queues, event_log = load_rules(
         "examples/23_nested_data.yml"
     )
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(root=dict(nested=dict(i=0))))
     queue.put_nowait(dict(root=dict(nested=dict(i=1))))
     queue.put_nowait(Shutdown())
@@ -597,13 +619,14 @@ async def test_23_nested_data():
 
 @pytest.mark.asyncio
 async def test_24_max_attributes():
-    ruleset_queues, queue, event_log = load_rules(
+    ruleset_queues, event_log = load_rules(
         "examples/24_max_attributes.yml"
     )
 
     with open("examples/replays/24_max_attributes/00.json") as f:
         data = json.loads(f.read())
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(data)
     queue.put_nowait(Shutdown())
 
@@ -625,13 +648,14 @@ async def test_24_max_attributes():
 
 @pytest.mark.asyncio
 async def test_25_max_attributes_nested():
-    ruleset_queues, queue, event_log = load_rules(
+    ruleset_queues, event_log = load_rules(
         "examples/25_max_attributes_nested.yml"
     )
 
     with open("examples/replays/25_max_attributes_nested/00.json") as f:
         data = json.loads(f.read())
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(data)
     queue.put_nowait(Shutdown())
 
@@ -653,10 +677,11 @@ async def test_25_max_attributes_nested():
 
 @pytest.mark.asyncio
 async def test_26_print_events():
-    ruleset_queues, queue, event_log = load_rules(
+    ruleset_queues, event_log = load_rules(
         "examples/26_print_events.yml"
     )
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=1))
     queue.put_nowait(dict(i=2))
     queue.put_nowait(Shutdown())
@@ -681,8 +706,9 @@ async def test_26_print_events():
 
 @pytest.mark.asyncio
 async def test_27_var_root():
-    ruleset_queues, queue, event_log = load_rules("examples/27_var_root.yml")
+    ruleset_queues, event_log = load_rules("examples/27_var_root.yml")
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(
         dict(
             webhook=dict(
@@ -717,10 +743,11 @@ async def test_27_var_root():
 
 @pytest.mark.asyncio
 async def test_28_right_side_condition_template():
-    ruleset_queues, queue, event_log = load_rules(
+    ruleset_queues, event_log = load_rules(
         "examples/28_right_side_condition_template.yml"
     )
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait({"i": 1})
     queue.put_nowait({"i": 2})
     queue.put_nowait(Shutdown())
@@ -745,8 +772,9 @@ async def test_28_right_side_condition_template():
 
 @pytest.mark.asyncio
 async def test_29_run_module():
-    ruleset_queues, queue, event_log = load_rules("examples/29_run_module.yml")
+    ruleset_queues, event_log = load_rules("examples/29_run_module.yml")
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=1, meta=dict(hosts="localhost")))
     queue.put_nowait(Shutdown())
 
@@ -777,10 +805,11 @@ async def test_29_run_module():
 
 @pytest.mark.asyncio
 async def test_30_run_module_missing():
-    ruleset_queues, queue, event_log = load_rules(
+    ruleset_queues, event_log = load_rules(
         "examples/30_run_module_missing.yml"
     )
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=1, meta=dict(hosts="localhost")))
     queue.put_nowait(Shutdown())
 
@@ -811,10 +840,11 @@ async def test_30_run_module_missing():
 
 @pytest.mark.asyncio
 async def test_31_run_module_missing_args():
-    ruleset_queues, queue, event_log = load_rules(
+    ruleset_queues, event_log = load_rules(
         "examples/31_run_module_missing_args.yml"
     )
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=1, meta=dict(hosts="localhost")))
     queue.put_nowait(Shutdown())
 
@@ -845,10 +875,11 @@ async def test_31_run_module_missing_args():
 
 @pytest.mark.asyncio
 async def test_32_run_module_fail():
-    ruleset_queues, queue, event_log = load_rules(
+    ruleset_queues, event_log = load_rules(
         "examples/32_run_module_fail.yml"
     )
 
+    queue = ruleset_queues[0][1]
     queue.put_nowait(dict(i=1, meta=dict(hosts="localhost")))
     queue.put_nowait(Shutdown())
 
@@ -874,4 +905,96 @@ async def test_32_run_module_fail():
     assert event["type"] == "ProcessedEvent", "7"
     event = event_log.get_nowait()
     assert event["type"] == "Shutdown", "8"
+    assert event_log.empty()
+
+
+@pytest.mark.asyncio
+async def test_33_multiple_rulesets():
+    ruleset_queues, event_log = load_rules(
+        "examples/33_multiple_rulesets.yml"
+    )
+
+    queue = ruleset_queues[0][1]
+    queue.put_nowait(dict(i=1, meta=dict(hosts="localhost")))
+
+    queue = ruleset_queues[1][1]
+    queue.put_nowait(dict(i=1, meta=dict(hosts="localhost")))
+
+    queue.put_nowait(Shutdown())
+
+    await run_rulesets(
+        event_log,
+        ruleset_queues,
+        dict(),
+        dict(),
+    )
+
+    event = event_log.get_nowait()
+    assert event["type"] == "ProcessedEvent", "0"
+
+    event = event_log.get_nowait()
+    assert event["type"] == "Action", "1"
+    assert event["action"] == "assert_fact", "2"
+
+    event = event_log.get_nowait()
+    assert event["type"] == "Action", "3"
+    assert event["action"] == "noop", "4"
+
+    event = event_log.get_nowait()
+    assert event["type"] == "ProcessedEvent", "5"
+
+    event = event_log.get_nowait()
+
+    assert event["type"] == "Shutdown", "6"
+    assert event_log.empty()
+
+
+@pytest.mark.asyncio
+async def test_34_multiple_rulesets():
+    ruleset_queues, event_log = load_rules(
+        "examples/34_multiple_rulesets.yml"
+    )
+
+    queue = ruleset_queues[0][1]
+    queue.put_nowait(dict(i=1, meta=dict(hosts="localhost")))
+    queue.put_nowait(dict(i=2, meta=dict(hosts="localhost")))
+
+    queue = ruleset_queues[1][1]
+    queue.put_nowait(dict(i=1, meta=dict(hosts="localhost")))
+
+    queue.put_nowait(Shutdown())
+
+    await run_rulesets(
+        event_log,
+        ruleset_queues,
+        dict(),
+        dict(),
+    )
+
+    event = event_log.get_nowait()
+    assert event["type"] == "ProcessedEvent", "0"
+
+    event = event_log.get_nowait()
+    assert event["type"] == "Action", "1"
+    assert event["action"] == "assert_fact", "2"
+
+    event = event_log.get_nowait()
+    assert event["type"] == "ProcessedEvent", "5"
+
+    event = event_log.get_nowait()
+    assert event["type"] == "ProcessedEvent", "5"
+
+    event = event_log.get_nowait()
+    assert event["type"] == "Shutdown", "6"
+
+    event = event_log.get_nowait()
+    assert event["type"] == "Action", "3"
+    assert event["action"] == "noop", "4"
+
+    event = event_log.get_nowait()
+    assert event["type"] == "ProcessedEvent", "5"
+
+    event = event_log.get_nowait()
+
+    assert event["type"] == "Shutdown", "6"
     assert event_log.empty()
