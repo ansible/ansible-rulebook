5b853197079e73267c9b705c69b843b9d9b63ea0
Madhu Kanoor <mkanoor@redhat.com>
2022-10-12 12:09:58 -0400

    [AAP-4440] Support storing facts per host
    A ruleset can have an optional gather_facts flag which enables collection of
    ansible facts per host in the inventory list. These ansible facts are stored
    in the rules engine per host. When a rule matches the corresponding host
    fact and the event are returned from the rules engine.
    
    You can refer to the facts per host using the fact prefix. e.g.
    
    fact.meta.hosts == 'localhost'
    
    When the fact is returned from the rules engine it allows us to limit the hosts
    that the playbook would run on by using the meta.hosts key.
    
    https://issues.redhat.com/browse/AAP-4440

--- ansible_rulebook/util.py
+++ ansible_rulebook/util.py
@@ -1,7 +1,11 @@
+import glob
+import json
 import os
+import tempfile
 from pprint import pprint
 from typing import Any, Dict, List, Union
 
+import ansible_runner
 import jinja2
 import yaml
 
@@ -71,3 +75,38 @@ def json_count(data):
                 )
             for i in o.values():
                 q.append(i)
+
+
+def collect_ansible_facts(inventory: Dict) -> List[Dict]:
+    hosts_facts = []
+    with tempfile.TemporaryDirectory(
+        prefix="gather_facts"
+    ) as private_data_dir:
+        os.mkdir(os.path.join(private_data_dir, "inventory"))
+        with open(
+            os.path.join(private_data_dir, "inventory", "hosts"), "w"
+        ) as f:
+            f.write(yaml.dump(inventory))
+
+        r = ansible_runner.run(
+            private_data_dir=private_data_dir,
+            module="ansible.builtin.setup",
+            host_pattern="*",
+        )
+        if r.rc != 0:
+            raise Exception(
+                "Error collecting facts in ansible_runner.run "
+                f"rc={r.rc}, status={r.status}"
+            )
+
+        host_path = os.path.join(
+            private_data_dir, "artifacts", "*", "fact_cache", "*"
+        )
+        for host_file in glob.glob(host_path):
+            hostname = os.path.basename(host_file)
+            with open(host_file) as f:
+                data = json.load(f)
+            data["meta"] = dict(hosts=hostname)
+            hosts_facts.append(data)
+
+    return hosts_facts
