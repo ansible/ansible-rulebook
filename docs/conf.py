2a2f79d88e7e48f888fb45deda20f8fba4b9ff87
Alejandro Izquierdo <aizquier@redhat.com>
2022-11-17 12:57:35 +0100

    Add apache license header to all python files

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
+
+#  Copyright 2022 Red Hat, Inc.
 #
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 # ansible_rulebook documentation build configuration file, created by
 # sphinx-quickstart on Fri Jun  9 13:47:02 2017.
 #
@@ -19,7 +33,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 import ansible_rulebook
 
@@ -31,22 +46,22 @@ import ansible_rulebook
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = 'ansible-rulebook'
+project = "ansible-rulebook"
 copyright = "2022, Ben Thomasson"
 author = "Ben Thomasson"
 
@@ -64,15 +79,15 @@ release = ansible_rulebook.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'en'
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -83,7 +98,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -94,13 +109,13 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 # -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ansible_rulebookdoc'
+htmlhelp_basename = "ansible_rulebookdoc"
 
 
 # -- Options for LaTeX output ------------------------------------------
@@ -109,15 +124,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -127,9 +139,13 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass
 # [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'ansible_rulebook.tex',
-     'ansible-rulebook Documentation',
-     'Ben Thomasson', 'manual'),
+    (
+        master_doc,
+        "ansible_rulebook.tex",
+        "ansible-rulebook Documentation",
+        "Ben Thomasson",
+        "manual",
+    ),
 ]
 
 
@@ -138,9 +154,13 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'ansible_rulebook',
-     'ansible-rulebook Documentation',
-     [author], 1)
+    (
+        master_doc,
+        "ansible_rulebook",
+        "ansible-rulebook Documentation",
+        [author],
+        1,
+    )
 ]
 
 
@@ -150,13 +170,13 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'ansible_rulebook',
-     'ansible-rulebook Documentation',
-     author,
-     'ansible_rulebook',
-     'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "ansible_rulebook",
+        "ansible-rulebook Documentation",
+        author,
+        "ansible_rulebook",
+        "One line description of project.",
+        "Miscellaneous",
+    ),
 ]
-
-
-
