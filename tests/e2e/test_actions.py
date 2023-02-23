153262d29b560b724325bbbfa0c2983afff74a0c
Alejandro Izquierdo <aizquier@redhat.com>
2023-02-23 11:26:52 +0100

    AAP-8657 sanity test for multiple actions

--- tests/e2e/test_actions.py
+++ tests/e2e/test_actions.py
@@ -4,6 +4,7 @@ Module with tests for operators
 import logging
 import subprocess
 
+import jinja2
 import pytest
 from pytest_check import check
 
@@ -13,10 +14,11 @@ from .settings import SETTINGS
 LOGGER = logging.getLogger(__name__)
 DEFAULT_CMD_TIMEOUT = SETTINGS["cmd_timeout"]
 DEFAULT_SHUTDOWN_AFTER = SETTINGS["run_playbook_shutdown_after"]
+DEFAULT_EVENT_DELAY = SETTINGS["event_delay"]
 
 
 @pytest.mark.e2e
-def test_actions_sanity():
+def test_actions_sanity(update_environment):
     """
     A rulebook that contains multiple rules with the following list of actions:
         * run_playbook
@@ -29,10 +31,19 @@ def test_actions_sanity():
     Each rule has specific logic to be executed only one time
     and each action produces a specific output to be verified.
     """
+    env = update_environment(
+        {
+            "DEFAULT_SHUTDOWN_AFTER": str(DEFAULT_SHUTDOWN_AFTER),
+            "DEFAULT_EVENT_DELAY": str(DEFAULT_EVENT_DELAY),
+        }
+    )
+
     rulebook = (
         utils.BASE_DATA_PATH / "rulebooks/actions/test_actions_sanity.yml"
     )
-    cmd = utils.Command(rulebook=rulebook)
+    cmd = utils.Command(
+        rulebook=rulebook, envvars="DEFAULT_SHUTDOWN_AFTER,DEFAULT_EVENT_DELAY"
+    )
 
     LOGGER.info(f"Running command: {cmd}")
     result = subprocess.run(
@@ -41,57 +52,88 @@ def test_actions_sanity():
         capture_output=True,
         cwd=utils.BASE_DATA_PATH,
         text=True,
+        env=env,
     )
 
     assert result.returncode == 0
     assert not result.stderr
-
-    # assert each expected output per action tested
-    assert (
-        "Event matched: {'action': 'run_playbook'}" in result.stdout
-    ), "run_playbook action failed"
-
-    assert (
-        "Event matched: {'action': 'run_module'}" in result.stdout
-    ), "run_module action failed"
-
-    assert (
-        "{'action': 'print_event'}" in result.stdout
-    ), "print_event action failed"
-
-    event_debug_expected_output = """{'hosts': ['all'],
+    event_debug_expected_output_tpl = """kwargs:
+{'hosts': ['all'],
  'inventory': {'all': {'hosts': {'localhost': {'ansible_connection': 'local'}}}},
  'project_data_file': None,
  'ruleset': 'Test actions sanity',
  'source_rule_name': 'debug',
  'source_ruleset_name': 'Test actions sanity',
- 'variables': {'event': {'action': 'debug'}}}"""  # noqa: E501
+ 'variables': {'DEFAULT_EVENT_DELAY': '{{DEFAULT_EVENT_DELAY}}',
+               'DEFAULT_SHUTDOWN_AFTER': '{{DEFAULT_SHUTDOWN_AFTER}}',
+               'event': {'action': 'debug'}}}"""  # noqa: E501
 
-    assert event_debug_expected_output in result.stdout, "debug action failed"
+    event_debug_expected_output = jinja2.Template(
+        event_debug_expected_output_tpl
+    ).render(
+        DEFAULT_SHUTDOWN_AFTER=DEFAULT_SHUTDOWN_AFTER,
+        DEFAULT_EVENT_DELAY=DEFAULT_EVENT_DELAY,
+    )
+
+    # assert each expected output per action tested
+    with check:
+        assert (
+            "Event matched: {'action': 'run_playbook'}" in result.stdout
+        ), "run_playbook action failed"
+
+    with check:
+        assert (
+            "Event matched: {'action': 'run_module'}" in result.stdout
+        ), "run_module action failed"
+
+    with check:
+        assert (
+            "{'action': 'print_event'}" in result.stdout
+        ), "print_event action failed"
+
+    with check:
+        assert (
+            event_debug_expected_output in result.stdout
+        ), "debug action failed"
+
+    with check:
+        assert (
+            "Event matched in same ruleset: sent" in result.stdout
+        ), "post_event action failed"
+
+    with check:
+        assert (
+            "Event matched in different ruleset: sent" in result.stdout
+        ), "post_event action across rulesets failed"
+
+    with check:
+        assert (
+            "Fact matched in same ruleset: sent" in result.stdout
+        ), "set_fact action failed"
+
+    with check:
+        assert (
+            "Fact matched in different ruleset: sent" in result.stdout
+        ), "set_fact action across rulesets failed"
+
+    with check:
+        assert (
+            "Retracted fact in same ruleset, this should not be printed"
+            not in result.stdout
+        ), "retract_fact action failed"
+
+    multiple_actions_expected_outputs = [
+        f"Multiple action #{n} triggered" for n in range(1, 3)
+    ]
+
+    with check:
+        assert all(
+            expected in result.stdout
+            for expected in multiple_actions_expected_outputs
+        ), "multiple actions failed"
 
     assert (
-        "Event matched in same ruleset: sent" in result.stdout
-    ), "post_event action failed"
-
-    assert (
-        "Event matched in different ruleset: sent" in result.stdout
-    ), "post_event action across rulesets failed"
-
-    assert (
-        "Fact matched in same ruleset: sent" in result.stdout
-    ), "set_fact action failed"
-
-    assert (
-        "Fact matched in different ruleset: sent" in result.stdout
-    ), "set_fact action across rulesets failed"
-
-    assert (
-        "Retracted fact in same ruleset, this should not be printed"
-        not in result.stdout
-    ), "retract_fact action failed"
-
-    assert (
-        len(result.stdout.splitlines()) == 42
+        len(result.stdout.splitlines()) == 48
     ), "unexpected output from the rulebook"
 
 
