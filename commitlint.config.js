d67ee8edbc703baed51be2974b55791ecdd97cae
Alex <aizquier@redhat.com>
2023-10-05 16:50:14 +0200

    ci: add configuration for commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,8 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'body-max-line-length': [2, 'always', '*'],
+        'body-max-line-length': [2, 'always', Infinity],
+        'subject-case': [0, 'always', 'lower-case'],
+        'body-case': [0, 'always', 'lower-case'],
     },
 };
