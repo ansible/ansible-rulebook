d3879321619afc8c2e907e8beae86ac2dddf74b4
Alex <aizquier@redhat.com>
2023-10-10 19:05:21 +0200

    fix: remove the rule for the bodycase

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,6 +4,5 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'body-max-line-length': [2, 'always', Infinity],
-        'body-case': [0, 'always', 'lower-case'],
     },
 };
