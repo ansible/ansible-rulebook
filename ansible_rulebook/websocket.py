ansible/ansible-rulebook

commit 00a7d72831aa935fa5f7d64834aa30d89bb5db66

refactor: extract create context to public func (#763)
Since both functions are very similar, refactor and move to util.

Signed-off-by: reidliu <reid201711@gmail.com>
Co-authored-by: reidliu <reid201711@gmail.com>

ansible_rulebook/websocket.py
@@ -18,7 +18,6 @@ import json
 import logging
 import os
 import random
-import ssl
 import tempfile
 import typing as tp
 from dataclasses import dataclass, field
@@ -31,7 +30,7 @@ from ansible_rulebook import rules_parser as rules_parser
 from ansible_rulebook.common import StartupArgs
 from ansible_rulebook.conf import settings
 from ansible_rulebook.token import renew_token
-from ansible_rulebook.util import validate_url
+from ansible_rulebook.util import create_context, validate_url
 from ansible_rulebook.vault import Vault, has_vaulted_str
 
 from .exception import InvalidUrlException
@@ -298,12 +297,5 @@ async def _handle_send_event_log(
         logs.event = None
 
 
-def _sslcontext() -> tp.Optional[ssl.SSLContext]:
-    if settings.websocket_url.startswith("wss"):
-        ssl_verify = settings.websocket_ssl_verify.lower()
-        if ssl_verify in ["yes", "true"]:
-            return ssl.create_default_context()
-        if ssl_verify in ["no", "false"]:
-            return ssl._create_unverified_context()
-        return ssl.create_default_context(cafile=ssl_verify)
-    return None
+def _sslcontext():
+    return create_context(settings.websocket_token_url, "wss")

ansible_rulebook/token.py
@@ -13,13 +13,12 @@
 #  limitations under the License.
 
 import logging
-import ssl
-import typing as tp
 
 import aiohttp
 
 from ansible_rulebook.conf import settings
 from ansible_rulebook.exception import TokenNotFound
+from ansible_rulebook.util import create_context
 
 logger = logging.getLogger(__name__)
 
@@ -44,12 +43,5 @@ async def renew_token() -> str:
             return data["access"]
 
 
-def _sslcontext() -> tp.Optional[ssl.SSLContext]:
-    if settings.websocket_token_url.startswith("https"):
-        ssl_verify = settings.websocket_ssl_verify.lower()
-        if ssl_verify in ["yes", "true"]:
-            return ssl.create_default_context()
-        if ssl_verify in ["no", "false"]:
-            return ssl._create_unverified_context()
-        return ssl.create_default_context(cafile=ssl_verify)
-    return None
+def _sslcontext():
+    return create_context(settings.websocket_token_url, "https")

ansible_rulebook/util.py
@@ -21,6 +21,7 @@ import os
 import platform
 import re
 import shutil
+import ssl
 import subprocess
 import sys
 import tempfile
@@ -424,3 +425,16 @@ def mask_sensitive_variable_values(
         return [mask_sensitive_variable_values(item) for item in obj]
     else:
         return obj
+
+
+def create_context(
+    settings_url: str, protocol_prefix: str
+) -> typing.Optional[ssl.SSLContext]:
+    if settings_url.startswith(protocol_prefix):
+        ssl_verify = settings.websocket_ssl_verify.lower()
+        if ssl_verify in ["yes", "true"]:
+            return ssl.create_default_context()
+        if ssl_verify in ["no", "false"]:
+            return ssl._create_unverified_context()
+        return ssl.create_default_context(cafile=ssl_verify)
+    return None

commit 9410cbc7864e6e8c8528a8f20067df34bf869d07

fix: fix url for websocket ssl context (#777)
Fix a bug introduced by
https://github.com/ansible/ansible-rulebook/pull/763 that prevents ws
connections.

Signed-off-by: Alex <aizquier@redhat.com>

ansible_rulebook/websocket.py
@@ -298,4 +298,4 @@ async def _handle_send_event_log(
 
 
 def _sslcontext():
-    return create_context(settings.websocket_token_url, "wss")
+    return create_context(settings.websocket_url, "wss")