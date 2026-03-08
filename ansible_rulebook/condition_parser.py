3b3aa042ebedcff12071fc9d61c49cc5dcf50999
Madhu Kanoor <mkanoor@redhat.com>
2026-03-08 19:47:15 -0400

    chore: deprecation warnings
    Fix the deprecation warnings
    
    ```
    ansible_rulebook/condition_parser.py:112
      /ansible_rulebook/condition_parser.py:112: PyparsingDeprecationWarning: 'originalTextFor' deprecated - use 'original_text_for'
        originalTextFor(QuotedString('"'))
    
    ansible_rulebook/condition_parser.py:113
      /ansible_rulebook/condition_parser.py:113: PyparsingDeprecationWarning: 'originalTextFor' deprecated - use 'original_text_for'
        | originalTextFor(QuotedString("'"))
    
    ansible_rulebook/condition_parser.py:149
      /ansible_rulebook/condition_parser.py:149: PyparsingDeprecationWarning: 'delimitedList' deprecated - use 'DelimitedList'
        + Group(Optional(delimitedList(string1 | string2 | varname | key_value)))
    
    ansible_rulebook/condition_parser.py:154
      /ansible_rulebook/condition_parser.py:154: PyparsingDeprecationWarning: 'delimitedList' deprecated - use 'DelimitedList'
        delimitedList(number_t | null_t | boolean | varname | string1 | string2)
    
    ansible_rulebook/condition_parser.py:161
      /ansible_rulebook/condition_parser.py:161: PyparsingDeprecationWarning: 'delimitedList' deprecated - use 'DelimitedList'
        + Group(delimitedList(allowed_values | list_values | varname))
    
    ansible_rulebook/condition_parser.py:168
      /ansible_rulebook/condition_parser.py:168: PyparsingDeprecationWarning: 'delimitedList' deprecated - use 'DelimitedList'
        + Group(delimitedList(allowed_values | list_values | varname))
    ```

--- ansible_rulebook/condition_parser.py
+++ ansible_rulebook/condition_parser.py
@@ -16,6 +16,7 @@ import logging
 
 from pyparsing import (
     Combine,
+    DelimitedList,
     Group,
     Keyword,
     Literal,
@@ -28,10 +29,9 @@ from pyparsing import (
     Word,
     ZeroOrMore,
     alphanums,
-    delimitedList,
     infix_notation,
     one_of,
-    originalTextFor,
+    original_text_for,
     pyparsing_common,
 )
 
@@ -109,8 +109,8 @@ varname = (
             | (
                 ("[")
                 + (
-                    originalTextFor(QuotedString('"'))
-                    | originalTextFor(QuotedString("'"))
+                    original_text_for(QuotedString('"'))
+                    | original_text_for(QuotedString("'"))
                     | pyparsing_common.signed_integer
                 )
                 + ("]")
@@ -146,26 +146,26 @@ key_value = ident + Suppress("=") + allowed_values
 string_search_t = (
     one_of("regex match search")
     + Suppress("(")
-    + Group(Optional(delimitedList(string1 | string2 | varname | key_value)))
+    + Group(Optional(DelimitedList(string1 | string2 | varname | key_value)))
     + Suppress(")")
 )
 
 delim_value = Group(
-    delimitedList(number_t | null_t | boolean | varname | string1 | string2)
+    DelimitedList(number_t | null_t | boolean | varname | string1 | string2)
 )
 list_values = Suppress("[") + delim_value + Suppress("]")
 
 selectattr_t = (
     Literal("selectattr")
     + Suppress("(")
-    + Group(delimitedList(allowed_values | list_values | varname))
+    + Group(DelimitedList(allowed_values | list_values | varname))
     + Suppress(")")
 )
 
 select_t = (
     Literal("select")
     + Suppress("(")
-    + Group(delimitedList(allowed_values | list_values | varname))
+    + Group(DelimitedList(allowed_values | list_values | varname))
     + Suppress(")")
 )
 
