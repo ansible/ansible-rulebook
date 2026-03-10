4dc69754496a7439abc00630ede2fba0ce0f9d0d
Madhu Kanoor <mkanoor@redhat.com>
2026-03-10 13:54:08 -0400

    feat: jinja custom filters in actions (#888)
    Added custom jinja filters which can be used in actions
    
    * regex_replace
    * basename
    * normpath
    * dirname
    https://issues.redhat.com/browse/AAP-68086

---
+++ ansible_rulebook/jinja/filters.py
@@ -0,0 +1,105 @@
+#  Copyright 2026 Red Hat, Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import os
+import re
+from typing import Any, Callable, Dict
+
+
+def regex_replace(
+    value: Any,
+    pattern: str,
+    replacement: str,
+    count: int = 0,
+    ignore_case: bool = False,
+    multiline: bool = False,
+    mandatory_count: int = 0,
+) -> str:
+    """
+    Perform a regex substitution with optional strictness via mandatory_count.
+    """
+    if value is None:
+        return ""
+
+    flags: int = 0
+    if ignore_case:
+        flags |= re.IGNORECASE
+    if multiline:
+        flags |= re.MULTILINE
+
+    # re.subn returns a Tuple[str, int]
+    new_value: str
+    actual_count: int
+    new_value, actual_count = re.subn(
+        pattern, replacement, str(value), count=count, flags=flags
+    )
+
+    if mandatory_count > 0 and actual_count < mandatory_count:
+        raise ValueError(
+            "Regex replace failed: expected at least "
+            f"{mandatory_count} replacements, "
+            f"but only found {actual_count}."
+        )
+
+    return new_value
+
+
+def basename(
+    value: Any,
+) -> str:
+    """
+    Get the basename from a file path
+    To get the last name of a file path, like 'cert.pem'
+    out of '/etc/certs/cert.pem'.
+    """
+    if value is None:
+        return ""
+
+    return os.path.basename(value)
+
+
+def dirname(
+    value: Any,
+) -> str:
+    """
+    Get the dirname from a file path
+    To get the dirname of a file path, like '/etc/certs'
+    out of '/etc/certs/cert.pem'.
+    if only the filename is passed in dirname will be
+    empty string
+    """
+    if value is None:
+        return ""
+
+    return os.path.dirname(value)
+
+
+def normpath(
+    value: Any,
+) -> str:
+    """
+    Get the normalized path from a file path
+    Extra slashes get dropped and get normalized
+    """
+    if value is None:
+        return ""
+
+    return os.path.normpath(value)
+
+
+FILTERS: Dict[str, Callable[..., str]] = {
+    "regex_replace": regex_replace,
+    "basename": basename,
+    "dirname": dirname,
+    "normpath": normpath,
+}
